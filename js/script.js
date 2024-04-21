b56ab343f308607a7b6403b09ead88aac4e9f092
rafaelabou1999 <rafaelabourdette@gmail.com>
2024-04-21 10:48:24 -0300

    missing responsive

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
 let startBox = document.querySelector('.start');
 let bright = document.querySelector(".bright");
-let btnBox = document.querySelector(".start button");
-
-function initGame(){
-    
-    bright.style.display = 'none';
-    startBox.style.display = 'none';
-}
-btnBox.addEventListener("click", () => initGame())
-
-
-
-
-
-
-
-
-
 
 
 let holes = document.querySelectorAll('.hole');
@@ -26,18 +9,33 @@ let random;
 
 let hearts = document.querySelectorAll('.hearts img');
 let heartScore = document.querySelector(".heartScore");
-
-
-
-let holesArray = Array.from(holes);
-let imgsArray = Array.from(imgs);
+let heartPoint = 5;
 
 let imgToScore;
 
 let earn100 = document.querySelectorAll(".earn100");
 let earn200 = document.querySelectorAll(".earn200");
 let earn = document.querySelectorAll(".earn");
+let scoreHtml = document.querySelector(".end span");
+let score = 0;
 
+let endBox = document.querySelector(".end");
+let btnEnd = document.querySelector(".end button");
+
+
+//Converting to array
+let holesArray = Array.from(holes);
+let imgsArray = Array.from(imgs);
+
+
+
+
+
+//=================== FUNCTIONS ===========================
+function initGame(){
+    bright.style.display = 'none';
+    startBox.style.display = 'none';
+}
 
 function clickMole(){
     random = Math.floor(Math.random() * holesArray.length);
@@ -45,37 +43,20 @@ function clickMole(){
         if(imgsArray.indexOf(img) === random){       
             img.style.display = 'block';
             imgToScore = img;
-            let triesNumber = parseFloat(tries.innerHTML); // Extract the current value
+            let triesNumber = parseFloat(tries.innerHTML);
             tries.innerHTML = triesNumber + 1; // 
             tries.style.fontSize = '1.2rem';
             
             function hideImage() {
-                img.style.display = "none"; // Set the display property to "none"
+                img.style.display = "none"; 
               }   
-            setTimeout(hideImage, 900);
+            setTimeout(hideImage, 1100);
         } else{
             img.style.display = 'none';
-        }
-        setScore(random);       
-    })   
-
-    
+        }    
+    })      
 }
 
-let score = 0;
-function setScore(random, img){
-    console.log('foi')
-    holesArray.forEach((hole) => {
-        imgsArray.forEach((img) => {
-           if(imgsArray.indexOf(img) === holesArray.indexOf(hole) === random){
-              
-           }
-        })
-    })
-    
-}
-
-let heartPoint = 5;
 function removeLife(){
     heartArray.forEach((heart) => {
       console.log("heart aq")
@@ -83,8 +64,9 @@ function removeLife(){
     })
 }
 
-let lostBox = document.querySelector(".lost");
-let btnLost = document.querySelector(".lost button");
+
+//=================== EVENTS ===========================
+startBox.addEventListener("click", () => initGame())
 
 holesArray.forEach((hole) => {
     hole.addEventListener("click", (e) =>{
@@ -96,15 +78,13 @@ holesArray.forEach((hole) => {
                     let earnArray = Array.from(earn)
                     earnArray[random].style.display = 'block';
                 })
-                if(hole.classList.contains("earn100")){
+                if(hole.classList.contains("e100")){
                     score += 100;
-                } else if(hole.classList.contains("earn200")){
+                } else if(hole.classList.contains("e200")){
                     score += 200;
                 }
-          console.log(score);
-            
-            console.log("GANHOUUUUUUUUUUUUU")
-           
+        
+                scoreHtml.innerHTML = score;
            } else{
            
             if (heartPoint > 0) {
@@ -116,8 +96,9 @@ holesArray.forEach((hole) => {
 
             if(heartPoint === 0){
                 bright.style.display = 'block';
-                lostBox.style.display = 'block';
+                endBox.style.display = 'block';
                 tries.innerHTML = 0;
+                score = 0;
             }
            }
         } 
@@ -125,15 +106,12 @@ holesArray.forEach((hole) => {
     } )
 })
 
-
-
-console.log(score);
-btnLost.addEventListener("click", () => {
+btnEnd.addEventListener("click", () => {
     heartPoint = 5;
     heartScore.innerText = heartPoint;
    
     bright.style.display = 'none';
-    lostBox.style.display = 'none';
+    endBox.style.display = 'none';
   
     hearts.forEach((heart) => {
         heart.src = "images/heartFull.png";

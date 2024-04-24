40bdaf6549384d40416c5567b74454329ffe2bbe
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2024-04-24 11:54:27 -0300

    changing commands in js

--- js/script.js
+++ js/script.js
@@ -1,122 +1,118 @@
 let startBox = document.querySelector('.start');
-let bright = document.querySelector(".bright");
+let bright= document.querySelector(".bright");
 
 
 let holes = document.querySelectorAll('.hole');
-let imgs = document.querySelectorAll('.character');
+let character = document.querySelectorAll('.character');
 let tries = document.querySelector('.tries');
 let random;
 
 let hearts = document.querySelectorAll('.hearts img');
-let heartScore = document.querySelector(".heartScore");
-let heartPoint = 5;
+let lifeAmount= document.querySelector(".lifeAmount");
+let totalOfLives= 5;
 
 let imgToScore;
 
-let earn100 = document.querySelectorAll(".earn100");
-let earn200 = document.querySelectorAll(".earn200");
-let earn = document.querySelectorAll(".earn");
+let toEarn = document.querySelectorAll(".earn");
 let scoreHtml = document.querySelector(".end span");
-let score = 0;
+let finalScore = 0;
 
 let endBox = document.querySelector(".end");
 let btnEnd = document.querySelector(".end button");
 
-
-//Converting to array
-let holesArray = Array.from(holes);
-let imgsArray = Array.from(imgs);
+let holeArray = Array.from(holes);
+let characterArray = Array.from(character);
 
 
-
-
-
-//=================== FUNCTIONS ===========================
 function initGame(){
     bright.style.display = 'none';
     startBox.style.display = 'none';
 }
 
-function clickMole(){
-    random = Math.floor(Math.random() * holesArray.length);
-    imgsArray.forEach((img) => {
-        if(imgsArray.indexOf(img) === random){       
-            img.style.display = 'block';
-            imgToScore = img;
-            let triesNumber = parseFloat(tries.innerHTML);
-            tries.innerHTML = triesNumber + 1; // 
-            tries.style.fontSize = '1.2rem';
-            
-            function hideImage() {
-                img.style.display = "none"; 
-              }   
-            setTimeout(hideImage, 1100);
-        } else{
-            img.style.display = 'none';
-        }    
-    })      
-}
+function hideImage() {
+    this.style.display = "none"; 
+}   
 
 function removeLife(){
-    heartArray.forEach((heart) => {
-    console.log(--heartPoint);
-    })
+    Array.prototype.map(hearts, heart => --totalOfLives);
 }
 
 
-//=================== EVENTS ===========================
+function clickMole(){
+	random = Math.floor(Math.random() * holeArray.length);
+	characterArray.forEach((img) => {
+		 if(characterArray.indexOf(img) === random){       
+			  img.style.display = 'block';
+			  imgToScore = img;
+			  let triesNumber = parseFloat(tries.innerHTML);
+			  tries.innerHTML = triesNumber + 1; // 
+			  tries.style.fontSize = '1.2rem';
+			  
+			  function hideImage() {
+					img.style.display = "none"; 
+				 }   
+			  setTimeout(hideImage, 1100);
+		 } else{
+			  img.style.display = 'none';
+		 }    
+	})      
+
+}
+
+
 startBox.addEventListener("click", () => initGame())
 
-holesArray.forEach((hole) => {
+holeArray.forEach((hole) => {
     hole.addEventListener("click", (e) =>{
         clickMole();
         if(hole){
-           if(holesArray.indexOf(hole) === random){
+           const earnArray = Array.from(toEarn);
+           const correctHole = holeArray.indexOf(hole) === random;
             
-                earn.forEach((each) => {
-                    let earnArray = Array.from(earn)
-                    earnArray[random].style.display = 'block';
-                })
-                if(hole.classList.contains("e100")){
-                    score += 100;
-                } else if(hole.classList.contains("e200")){
-                    score += 200;
-                }
-        
-                scoreHtml.innerHTML = score;
-           } else{
-            earn.forEach((each) => {
-                let earnArray = Array.from(earn)
-                earnArray[random].style.display = 'none';
-            })
-            if (heartPoint > 0) {
-                heartPoint -= 1;
-                heartScore.innerText = heartPoint;
-                hearts[heartPoint].src = "images/emptyheart.png";
-                
+           if(correctHole){   
+					if (correctHole) {
+						earnArray[random].style.display = 'block';
+						if (hole.classList.contains("e100")) {
+							finalScore += 100;
+						} else if (hole.classList.contains("e200")) {
+							finalScore += 200;
+						} else{
+							finalScore += 0;
+						}
+
+            scoreHtml.innerHTML = finalScore;         
+				console.log("correct hole " + finalScore)
+           } 
+
+            if (totalOfLives > 0) {
+					 totalOfLives -= 1;
+                lifeAmount.innerText = totalOfLives;
+                hearts[totalOfLives].src = "images/emptyheart.png";    
             }
 
-            if(heartPoint === 0){
+            if(totalOfLives === 0){
                 bright.style.display = 'block';
                 endBox.style.display = 'block';
                 tries.innerHTML = 0;
-                score = 0;
+                finalScore = 0;
             }
-           }
+          }
         } 
-       
-    } )
 })
+   }
+)
 
 btnEnd.addEventListener("click", () => {
-    heartPoint = 5;
-    heartScore.innerText = heartPoint;
-   
+	 totalOfLives = 5;
+    lifeAmount.innerText = totalOfLives;
+    finalScore = 0;
+	 console.log('Final' + finalScore)
     bright.style.display = 'none';
     endBox.style.display = 'none';
   
     hearts.forEach((heart) => {
         heart.src = "images/heartFull.png";
     })
-})
+  }  
+)
 
